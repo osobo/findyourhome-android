49474caefea47d57b9cc25b7ff8e250a08a5d4c2
osobo <oberglof@kth.se>
2017-05-16 20:55:29 +0200

    Listings can now be (un)favorited from the details

--- app/src/main/java/se/findyourhome/findyourhomeapp/ListingDetailsActivity.java
+++ app/src/main/java/se/findyourhome/findyourhomeapp/ListingDetailsActivity.java
@@ -1,5 +1,6 @@
 package se.findyourhome.findyourhomeapp;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -23,7 +24,11 @@ public class ListingDetailsActivity extends AppCompatActivity {
     private TextView pubDateView;
     private TextView areaView;
 
+    private ImageView favIconView;
+
     private String url;
+
+    private boolean isFavorite = false;
 
 
     @Override
@@ -39,14 +44,44 @@ public class ListingDetailsActivity extends AppCompatActivity {
         pubDateView = (TextView) this.findViewById(R.id.pubDateView);
         areaView = (TextView) this.findViewById(R.id.areaView);
 
-        Intent intent = getIntent();
-        String url = intent.getStringExtra(URL_EXTRA_ARG);
+        favIconView = (ImageView) this.findViewById(R.id.favIcon);
 
-        String query = "SELECT * FROM " + ListingDbContract.Listing.TABLE_NAME +
-            " WHERE " + ListingDbContract.Listing.COLUMN_NAME_URL + " = '" + url + "'";
+        Intent intent = getIntent();
+        this.url = intent.getStringExtra(URL_EXTRA_ARG);
+
+        favIconView.setOnClickListener(new ToggleFavOnCLick());
 
         ListingDbHelper helper = new ListingDbHelper(this);
         SQLiteDatabase db = helper.getReadableDatabase();
+
+        insertListingInfo(db);
+        checkFavorite(db);
+
+        db.close();
+    }
+
+    /**
+     * By default this activity shows the listing as not be favorited. This methods checks if
+     * the listing is favorited and changes to 'favIcon' to reflect that if so.
+     * @param db The db to use to check if the listing is favorited.
+     */
+    private void checkFavorite(SQLiteDatabase db) {
+        String query = "SELECT * FROM " + ListingDbContract.Favorite.TABLE_NAME +
+                " WHERE " + ListingDbContract.Favorite.COLUMN_NAME_URL + " = '" + url + "'";
+
+        Cursor c = db.rawQuery(query, null);
+
+        if(c.moveToFirst()) {
+            // If we could move to first there was a result => The listing is a favorite.
+            setFavoriteIcon();
+        }
+        // Else: do nothing as the default is correct.
+    }
+
+    private void insertListingInfo(SQLiteDatabase db) {
+        String query = "SELECT * FROM " + ListingDbContract.Listing.TABLE_NAME +
+                " WHERE " + ListingDbContract.Listing.COLUMN_NAME_URL + " = '" + url + "'";
+
         Cursor c = db.rawQuery(query, null);
 
         if(!c.moveToFirst()) {
@@ -56,7 +91,7 @@ public class ListingDetailsActivity extends AppCompatActivity {
             this.url = listing.url;
 
             imgView.setImageBitmap(BitmapFactory.decodeByteArray(listing.image, 0,
-                listing.image.length));
+                    listing.image.length));
             addressView.setText(listing.address);
             priceView.setText(listing.price + " / month");
             pubDateView.setText(listing.pubDate);
@@ -64,10 +99,38 @@ public class ListingDetailsActivity extends AppCompatActivity {
             areaView.setText(listing.area);
             contractView.setText(listing.contract);
         }
+    }
+
+    private void setFavoriteIcon() {
+        favIconView.setImageResource(R.mipmap.favorite_icon);
+    }
+
+    private void unsetFavoriteIcon() {
+        favIconView.setImageResource(R.mipmap.not_favorite_icon);
+    }
+
+    private void setFavoriteStat() {
+        System.out.println("Setting fav " + url);
+        ListingDbHelper helper = new ListingDbHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        ContentValues cvs = new ContentValues();
+        cvs.put(ListingDbContract.Favorite.COLUMN_NAME_URL, url);
+        db.insert(ListingDbContract.Favorite.TABLE_NAME, null, cvs);
 
         db.close();
     }
 
+    private void unsetFavoriteStat() {
+        System.out.println("Unsetting fav " + url);
+        ListingDbHelper helper = new ListingDbHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        String whereClause = ListingDbContract.Favorite.COLUMN_NAME_URL + " = '" + url + "'";
+        db.delete(ListingDbContract.Favorite.TABLE_NAME, whereClause, null);
+
+        db.close();
+    }
 
     private void onInvalidUrl() {
         // TODO
@@ -87,5 +150,22 @@ public class ListingDetailsActivity extends AppCompatActivity {
         startActivity(gotoSiteIntent);
     }
 
+    public void toggleFavorite() {
+        if(isFavorite) {
+            unsetFavoriteIcon();
+            unsetFavoriteStat();
+        } else {
+            setFavoriteIcon();
+            setFavoriteStat();
+        }
+        isFavorite = !isFavorite;
+    }
+
+    private class ToggleFavOnCLick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            toggleFavorite();
+        }
+    }
 
 }
